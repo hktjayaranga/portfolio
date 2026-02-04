490f0a5d31729cf402ec9dce5e26be5477c47379
hktjayaranga <hktjayaranga@gmail.com>
2026-02-04 20:23:19 +0530

    add SEO tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,31 @@ const monoFont = IBM_Plex_Mono({
 
 export const metadata: Metadata = {
   title: 'Thilina Jayaranga | Portfolio',
-  description: 'Personal portfolio of Thilina Jayaranga, Associate Software Engineering.'
+  description:
+    'Official portfolio of Thilina Jayaranga, Computer Science undergraduate and software developer.',
+  metadataBase: new URL('https://thilinaj.me'),
+  alternates: {
+    canonical: '/'
+  },
+  openGraph: {
+    title: 'Thilina Jayaranga | Portfolio',
+    description:
+      'Official portfolio of Thilina Jayaranga, Computer Science undergraduate and software developer.',
+    url: '/',
+    siteName: 'Thilina Jayaranga Portfolio',
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Thilina Jayaranga | Portfolio',
+    description:
+      'Official portfolio of Thilina Jayaranga, Computer Science undergraduate and software developer.'
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
 };
 
 export default function RootLayout({
